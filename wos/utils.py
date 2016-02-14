678c3d2ca28cfeb895b68fd153854f6dc9ee22e3
enricobacis <enrico.bacis@gmail.com>
2016-02-14 20:19:34 +0100

    utils: pretty print xml

--- wos/utils.py
+++ wos/utils.py
@@ -3,14 +3,18 @@
 __all__ = ['doi_to_wos', 'query']
 
 from xml.etree import ElementTree as _ET
+from xml.dom import minidom as _minidom
 import re as _re
 
-def query(wosclient, wos_query, xml_query, count=5):
+def query(wosclient, wos_query, xml_query=None, count=5):
     """Query Web of Science and then XML query the results."""
     result = wosclient.search(wos_query, count=1)
     xml = _re.sub(' xmlns="[^"]+"', '', result.records, count=1)
-    results = _ET.fromstring(xml).findall(xml_query)
-    return [el.text for el in results]
+    if xml_query:
+        xml = _ET.fromstring(xml)
+        return [el.text for el in xml.findall(xml_query)]
+    else:
+        return _minidom.parseString(xml).toprettyxml()
 
 def doi_to_wos(wosclient, doi):
     """Convert DOI to WOS identifier."""
