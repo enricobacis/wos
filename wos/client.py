ef288d9efd3768959dfe02ccc1ced8ce13f193dc
enricobacis <enrico.bacis@gmail.com>
2016-02-14 20:38:38 +0100

    allow the reuse of SessionID

--- wos/client.py
+++ wos/client.py
@@ -6,7 +6,7 @@ import suds as _suds
 
 class WosClient():
     """Query the Web of Science.
-       You must provide username and password only to user premium WWS service.
+       You must provide user and password only to user premium WWS service.
 
        with WosClient() as wos:
            results = wos.search(...)"""
@@ -15,14 +15,15 @@ class WosClient():
     auth_url = base_url + '/esti/wokmws/ws/WOKMWSAuthenticate?wsdl'
     search_url = base_url + '/esti/wokmws/ws/WokSearch?wsdl'
 
-    def __init__(self, username=None, password=None):
-        """Create the SOAP clients. username and password for premium access."""
-        self._auth = _suds.client.Client(self.auth_url)
+    def __init__(self, user=None, password=None, SID=None, close_on_exit=True):
+        """Create the SOAP clients. user and password for premium access."""
         self._search = _suds.client.Client(self.search_url)
-        self._open = False
+        self._auth = _suds.client.Client(self.auth_url)
+        self._close_on_exit = close_on_exit
+        self._SID = SID
 
-        if username and password:
-            authorization = ('%s:%s' % (username, password)).encode('base64')
+        if user and password:
+            authorization = ('%s:%s' % (user, password)).encode('base64')
             headers = {'Authorization': ('Basic %s' % authorization).strip()}
             self._auth.set_options(headers=headers)
 
@@ -33,28 +34,32 @@ class WosClient():
 
     def __exit__(self, exc_type, exc_value, traceback):
         """Close connection after closing the 'with' statement."""
-        self.close()
+        if self._close_on_exit:
+            self.close()
 
     def __del__(self):
         """Close connection when deleting the object."""
-        self.close()
+        if self._close_on_exit:
+            self.close()
 
     def connect(self):
         """Authenticate to WOS and set the SID cookie."""
-        SID = self._auth.service.authenticate()
-        self._search.set_options(headers={'Cookie': 'SID="%s"' % SID})
-        self._auth.options.headers.update({'Cookie': 'SID="%s"' % SID})
-        self._open = True
+        if not self._SID:
+            self._SID = self._auth.service.authenticate()
+
+        print 'Authenticated using SID:', self._SID
+        self._search.set_options(headers={'Cookie': 'SID="%s"' % self._SID})
+        self._auth.options.headers.update({'Cookie': 'SID="%s"' % self._SID})
 
     def close(self):
         """Close the session."""
-        if self._open:
+        if self._SID:
             self._auth.service.closeSession()
-            self._open = False
+            self._SID = None
 
     def search(self, query, count=5):
         """Perform a query. Check the WOS documentation for v3 syntax."""
-        if not self._open:
+        if not self._SID:
             raise RuntimeError('Session not open. Invoke .connect() before.')
 
         qparams = {'databaseId': 'WOS',
