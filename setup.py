dcb706d3d38efda458c5515effdd8b9f8e7252e0
enricobacis <enrico.bacis@gmail.com>
2016-02-14 22:02:25 +0100

    add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-#with open('README.rst') as README:
-#    long_description = README.read()
-#    long_description = long_description[long_description.index('Description'):]
+with open('README.rst') as README:
+    long_description = README.read()
+    long_description = long_description[long_description.index('Description'):]
 
 setup(name='wos',
-      version='0.1',
+      version='0.1.1',
       description='Web of Science client using API v3.',
-#      long_description=long_description,
+      long_description=long_description,
       install_requires=['suds'],
       url='http://github.com/enricobacis/wos',
       author='Enrico Bacis',
