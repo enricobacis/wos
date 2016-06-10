646864b92814bfb9263662ced78805e00c985eb8
enricobacis <enrico.bacis@gmail.com>
2016-06-10 09:56:17 +0200

    use a uglier but more portable way to detect python version

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
+from sys import version_info
 
 with open('README.rst') as README:
     long_description = README.read()
     long_description = long_description[long_description.index('Description'):]
 
+install_requires = ['suds'] if version_info < (3,0) else ['suds-py3']
+
 setup(name='wos',
       version='0.1.7',
       description='Web of Science client using API v3.',
       long_description=long_description,
-      extras_require={
-          ':python_version <= "2.7"': ['suds'],
-          ':python_version >= "3.0"': ['suds-py3'],
-      },
+      install_requires=install_requires,
       url='http://github.com/enricobacis/wos',
       author='Enrico Bacis',
       author_email='enrico.bacis@gmail.com',
