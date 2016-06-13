c92700b1331cc75cb8750e71c23df520a7c30eb2
enricobacis <enrico.bacis@gmail.com>
2016-06-13 15:13:57 +0200

    separate query and single methods

--- wos/utils.py
+++ wos/utils.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python
 
-__all__ = ['doi_to_wos', 'query']
+__all__ = ['doi_to_wos', 'query', 'single']
 
 from xml.etree import ElementTree as _ET
 from xml.dom import minidom as _minidom
 import re as _re
 
-def query(wosclient, wos_query, xml_query=None, count=5, offset=1):
-    """Query Web of Science and then XML query the results."""
-    # wos returns 100 documents metadata per query.
-    limit_per_query=100
-    results = []
-
-    _count = count
-    _offset = offset
-
-    while _count > 0:
-        num_results = min(limit_per_query, _count)
-        result = wosclient.search(wos_query, num_results, _offset)
-        _count -= num_results
-        _offset += num_results
-
-        xml = _re.sub(' xmlns="[^"]+"', '', result.records, count=1)
-        results.append(xml)
-
-    # remove <records> </records> tags and concat the result with them later
-    results = [i.replace('<records>', '') for i in results]
-    results = [i.replace('</records>', '') for i in results]
-    results = u'<records>\n' + reduce (lambda s, elem: s + elem + '\n', results, u'') + u'</records>'
-
+def single(wosclient, wos_query, xml_query=None, count=5, offset=1):
+    """Perform a single Web of Science query and then XML query the results."""
+    result = wosclient.search(wos_query, count, offset)
+    xml = _re.sub(' xmlns="[^"]+"', '', result.records, count=1)
     if xml_query:
-        xml = _ET.fromstring(results)
+        xml = _ET.fromstring(xml)
         return [el.text for el in xml.findall(xml_query)]
     else:
-        return _minidom.parseString(results).toprettyxml()
+        return _minidom.parseString(xml).toprettyxml()
+
+def query(wosclient, wos_query, xml_query=None, count=5, offset=1, limit=100):
+    """Query Web of Science and XML query results with multiple requests."""
+    results = [single(wosclient, wos_query, xml_query, min(limit, count-x+1), x)
+               for x in range(offset, count+1, limit)]
+    if xml_query:
+        return [el for res in results for el in res]
+    else:
+        pattern = _re.compile(r'.*?<records>|</records>.*', _re.DOTALL)
+        return ('<?xml version="1.0" ?>\n<records>' +
+                '\n'.join(pattern.sub('', res) for res in results) +
+                '</records>')
 
 def doi_to_wos(wosclient, doi):
     """Convert DOI to WOS identifier."""
