07db055069041394703a84f7b0308fddf3ae2353
enricobacis <enrico.bacis@gmail.com>
2020-03-12 15:05:03 +0100

    make readthedocs happier

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,9 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Static paths.
+html_static_path = []
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
