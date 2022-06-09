128f167fb705b1269815acd58c7f943b0bd0edf8
Scott <13770321+scott-8@users.noreply.github.com>
2022-06-09 13:03:29 -0400

    Format setup block, add environment markers for suds package

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup
-from sys import version_info
 
-with open('README.rst') as README:
+with open("README.rst") as README:
     long_description = README.read()
-    long_description = long_description[long_description.index('Description'):]
+    long_description = long_description[long_description.index("Description") :]
 
-suds_install_requires = ['suds'] if version_info < (3, 0) else ['suds-py3']
-
-setup(name='wos',
-      version='0.2.7',
-      description='Web of Science client using API v3.',
-      long_description=long_description,
-      install_requires=['limit'] + suds_install_requires,
-      url='http://github.com/enricobacis/wos',
-      author='Enrico Bacis',
-      author_email='enrico.bacis@gmail.com',
-      license='MIT',
-      packages=['wos'],
-      scripts=['scripts/wos'],
-      keywords='wos isi web of science knowledge api client')
+setup(
+    name="wos",
+    version="0.2.7",
+    description="Web of Science client using API v3.",
+    long_description=long_description,
+    install_requires=[
+        "limit",
+        'suds;python_version<"3.0"',
+        'suds-py3;python_version>="3.0"',
+    ],
+    url="http://github.com/enricobacis/wos",
+    author="Enrico Bacis",
+    author_email="enrico.bacis@gmail.com",
+    license="MIT",
+    packages=["wos"],
+    scripts=["scripts/wos"],
+    keywords="wos isi web of science knowledge api client",
+)
